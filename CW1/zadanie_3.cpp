8fe63a9bd1a0c53537929bbfa6a72f70660b18f1
Krzychulec <criskbp.2019@gmail.com>
2026-04-12 19:27:20 +0200

    Zadanie1.3

--- CW1/zadanie_3.cpp
+++ CW1/zadanie_3.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+
 int main()
 {
-
+    int n,s;
+    int dl[n];
+    cin >> n >> s;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> dl[i];
+    }
+    vector<int> dp(n,INT_MAX);
+    dp[0] = 0;
+    for(int
+         i=1; i<n; i++)
+    {
+        int zsumowana_dl = dl[i];
+        for(int j=0; j<i; j++)
+        {
+            if(zsumowana_dl <= s)
+            {
+                int koszt = s - zsumowana_dl;
+                dp[i] = min(dp[i], dp[i-j] + koszt*koszt);
+            }
+            zsumowana_dl += dl[i-j];
+        }
+    }
+    cout << dp[n-1] << endl;
+    return 0;
 }
