027f5b5044eac09328631f6b4dd4dfe42b9655a9
Krzychulec <criskbp.2019@gmail.com>
2026-04-29 23:43:16 +0200

    zadanie laby 1

--- LAB9/najwieksza_klika/najwieksza_klika/Lab09.cs
+++ LAB9/najwieksza_klika/najwieksza_klika/Lab09.cs
@@ -7,6 +7,38 @@ using ASD.Graphs;
 /// </summary>
 public static class Lab10GraphExtender
 {
+    ///<summary>
+    /// funkcja pomocnicza do rekurencyjnego liczenia maksymalnej kliki
+    /// </summary>
+    /// <param name="k">od którego wierzchołka możemy sprawdzać</param>
+    /// <param name="S">zbior wierzcholkow obecnie w klice</param>
+    /// <param name="maxClique">najwieksza klika do tej pory</param>
+    static int MaxCliqueRec(int k, List<int> S, int maxClique, int n, Graph g, ref int[] bestClique)
+    {
+        var C = new List<int>();
+        for (int i = k; i < n; i++)
+        {
+            if (S.All(s => g.HasEdge(i, s)))
+                C.Add(i);
+        }
+        
+        if (S.Count + C.Count <= maxClique)
+            return maxClique;
+        
+        if (S.Count > maxClique)
+        {
+            maxClique = S.Count;
+            bestClique = S.ToArray();
+        }
+
+        foreach (int vert in C)
+        {
+            S.Add(vert);                                                    
+            maxClique = MaxCliqueRec(vert + 1, S, maxClique, n, g, ref bestClique);
+            S.Remove(vert);                                                 
+        }
+        return maxClique;
+    }
     /// <summary>
     /// Wyznacza największą klikę w grafie i jej rozmiar metodą pełnego przeglądu (backtracking)
     /// </summary>
@@ -16,10 +48,13 @@ public static class Lab10GraphExtender
     /// <remarks>
     /// Nie wolno modyfikować badanego grafu.
     /// </remarks>
+    
     public static int MaxClique(this Graph g, out int[] clique)
     {
-        clique = null;
-        return 0;
+        clique = new int[0];
+        var S = new List<int>();
+        MaxCliqueRec(0, S, 0, g.VertexCount, g, ref clique);
+        return clique.Length;
     }
 
     /// <summary>
@@ -33,8 +68,55 @@ public static class Lab10GraphExtender
     /// 1) Uwzględniamy wagi krawędzi
     /// 3) Nie wolno modyfikować badanych grafów.
     /// </remarks>
+    ///
+    static bool IsConsistent(int k, int v, int[] map, Graph<int> g, Graph<int> h)
+    {
+        // Stopnie muszą się zgadzać
+        if (g.OutNeighbors(v).Count() != h.OutNeighbors(k).Count()) return false;
+
+        for (int j = 0; j < k; j++)
+        {
+            bool hMaKrawedz = h.HasEdge(k, j);
+            bool gMaKrawedz = g.HasEdge(v, map[j]);
+
+            if (hMaKrawedz != gMaKrawedz) return false;
+
+            if (hMaKrawedz)
+            {
+                if (h.GetEdgeWeight(k, j) != g.GetEdgeWeight(v, map[j]))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static bool GeneratePermutations(int n, int k, bool[] uzyte, Graph<int> g, Graph<int> h, int[] map)
+    {
+        if (k == n) return true;
+        for (int i = 0; i < n; i++)
+        {
+            if (uzyte[i]) continue;
+            if (IsConsistent(k, i, map, g, h))
+            {
+                map[k] = i;
+                uzyte[i] = true;
+                if (GeneratePermutations(n, k + 1, uzyte, g, h, map))
+                    return true;
+                uzyte[i] = false;
+            }
+        }
+        return false;
+    }
+
     public static bool IsomorphismTest(this Graph<int> g, Graph<int> h, out int[] map)
     {
+        map = null;
+        if (g.VertexCount != h.VertexCount) return false;
+        int n = g.VertexCount;
+        bool[] uzyte = new bool[n];
+        map = new int[n];
+        if (GeneratePermutations(n, 0, uzyte, g, h, map))
+            return true;
         map = null;
         return false;
     }
--- LAB9/najwieksza_klika/najwieksza_klika/obj/Debug/net9.0/najwieksza_klika.AssemblyInfo.cs
+++ LAB9/najwieksza_klika/najwieksza_klika/obj/Debug/net9.0/najwieksza_klika.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("najwieksza_klika")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f48adeac5e4afb229138e412e9c2e818b5fe57c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d8bf3a99824c62e45fcf1604b575b018349f57")]
 [assembly: System.Reflection.AssemblyProductAttribute("najwieksza_klika")]
 [assembly: System.Reflection.AssemblyTitleAttribute("najwieksza_klika")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
